7d108b873b2e836fe3c1afc83c7b68aba9da8ef7
wfyhehe <826487285@qq.com>
2022-11-11 03:03:35 -0500

    header and banner css

--- react/stackoverflow/src/layout/CenterPage.js
+++ react/stackoverflow/src/layout/CenterPage.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {Layout} from 'antd';
 import Router from "./Router";
 import About from "../component/About";
+import './styles/CenterPage.css';
 
 const {Header, Content, Sider} = Layout;
 export default class CenterPage extends Component {
@@ -9,8 +10,8 @@ export default class CenterPage extends Component {
     return (
       <div className="stackoverflow-center-page">
         <Layout>
-          <Header>
-            banner
+          <Header className="stackoverflow-center-page-banner-wrapper">
+            <img className="stackoverflow-center-page-banner-image" src="/banner.jpg" alt="banner"/>
           </Header>
           <Content>
             <Router />
--- react/stackoverflow/src/layout/HomeLayout.js
+++ react/stackoverflow/src/layout/HomeLayout.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {Layout} from 'antd';
 import CenterPage from "./CenterPage";
 import {Link} from "react-router-dom";
+import './styles/HomeLayout.css';
 
 const {Header, Content} = Layout;
 export default class HomeLayout extends Component {
@@ -9,10 +10,8 @@ export default class HomeLayout extends Component {
     return (
       <div className="stackoverflow-home-layout">
         <Layout>
-          <Header>
-            <div className="stackoverflow-home-layout-header">
-              <Link to="signin">Ask a question</Link>
-            </div>
+          <Header className="stackoverflow-home-layout-header">
+            <Link className="stackoverflow-home-layout-header-link" to="signin">Ask a question</Link>
           </Header>
           <Content>
             <CenterPage />
